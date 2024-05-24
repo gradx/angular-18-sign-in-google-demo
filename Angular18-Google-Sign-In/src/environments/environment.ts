899b82c75406fc31de6b7de39189a6953bcdbb05
BLUEDEVIL\gradx <gradx@hotmail.com>
2024-05-24 12:33:17 -0400

    remove unnecessary line

--- Angular18-Google-Sign-In/src/environments/environment.ts
+++ Angular18-Google-Sign-In/src/environments/environment.ts
@@ -3,4 +3,3 @@ export const environment = {
     google_client_id: '222000842037-qrfms25as5b680051uia4u6g3t60bgef.apps.googleusercontent.com',
     google_redirect_uri: 'https://localhost:7109/login-result'
 };
-7109
